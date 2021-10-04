c836dba2d09c43d5b0ff404a0ee21d017f8a58df
albe <a.beirith@wertgarantie.com>
2021-10-04 10:55:39 +0200

    add build

--- build/build.sh
+++ build/build.sh
@@ -5,31 +5,33 @@ set -eo pipefail
 BUILD_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 PROJECT_DIR="$(dirname "${BUILD_DIR}")";
 
-cd "${PROJECT_DIR}" || exit
-
 # create directories
 mkdir -p var/log var/cache
 chmod 777 var/log var/cache
 
-cd "${BUILD_DIR}" || exit
+COMPOSER_CACHE_VOLUME=composer-cache
+COMPOSER_IMAGE="composer"
+COMPOSER_TAG="2.1.8"
 
 # install composer
-chmod +x composer.sh
-./composer.sh install \
-    --prefer-dist \
-    --no-progress \
-    --no-interaction \
-    --optimize-autoloader
+docker run --rm -i -t \
+    -v ${CACHE_VOLUME}:/tmp \
+    -v "${PROJECT_DIR}":/app \
+    --user "$(id -u)":"$(id -g)" \
+    -w="/app" \
+    --entrypoint="/bin/bash" \
+    "${COMPOSER_IMAGE}:${COMPOSER_TAG}" \
+    -c "composer global require hirak/prestissimo &> /dev/null; composer install --prefer-dist --no-progress --no-interaction --optimize-autoloader"
 
 # build images
-cd "${PROJECT_DIR}/infrastructure" || exit
-docker-compose -f docker-compose.yml build
+#cd "${PROJECT_DIR}/infrastructure" || exit
+#docker-compose -f docker-compose.yml build
 
 # run container
-cd "${PROJECT_DIR}" || exit
-docker-compose up -d
+#cd "${PROJECT_DIR}" || exit
+#docker-compose up -d
 
 # run phpunit
-cd "${BUILD_DIR}" || exit
-chmod +x phpunit.sh
-./phpunit.sh
+#cd "${BUILD_DIR}" || exit
+#chmod +x phpunit.sh
+#./phpunit.sh
