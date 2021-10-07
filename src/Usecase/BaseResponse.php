0cac4f6418f8f4d5d4bbe8547501b45130e5ad14
albe <a.beirith@wertgarantie.com>
2021-10-07 15:23:15 +0200

    add some tests

--- src/Usecase/BaseResponse.php
+++ src/Usecase/BaseResponse.php
@@ -2,6 +2,7 @@
 
 namespace App\Usecase;
 
+use JetBrains\PhpStorm\ArrayShape;
 use Symfony\Component\HttpFoundation\Response;
 
 class BaseResponse
@@ -25,29 +26,14 @@ class BaseResponse
      */
     public function getHttpStatus(): int
     {
-        switch ($this->code) {
-            case ResultCodes::SUCCESS:
-            case ResultCodes::USER_NOT_FOUND:
-            case ResultCodes::DATABASE_IS_EMPTY:
-                return Response::HTTP_OK;
-            case ResultCodes::SUCCESS_CREATED;
-                return Response::HTTP_CREATED;
-            case ResultCodes::SUCCESS_NO_CONTENT:
-                return Response::HTTP_NO_CONTENT;
-            case ResultCodes::INVALID_JSON_CONTENT:
-            case ResultCodes::INVALID_SYNTAX:
-            case ResultCodes::USER_CAN_NOT_BE_UPDATED:
-            case ResultCodes::USER_CAN_NOT_BE_DELETED:
-            case ResultCodes::USER_CAN_NOT_BE_SAVED:
-                return Response::HTTP_BAD_REQUEST;
-            case ResultCodes::INVALID_MEDIA_TYPE:
-                return Response::HTTP_UNSUPPORTED_MEDIA_TYPE;
-            case ResultCodes::PDO_EXCEPTION_NO_LOGIN_DATA:
-            case ResultCodes::PDO_EXCEPTION:
-            case ResultCodes::UNKNOWN_ERROR:
-            default:
-                return Response::HTTP_INTERNAL_SERVER_ERROR;
-        }
+        return match ($this->code) {
+            ResultCodes::SUCCESS, ResultCodes::USER_NOT_FOUND, ResultCodes::DATABASE_IS_EMPTY => Response::HTTP_OK,
+            ResultCodes::SUCCESS_CREATED => Response::HTTP_CREATED,
+            ResultCodes::SUCCESS_NO_CONTENT => Response::HTTP_NO_CONTENT,
+            ResultCodes::INVALID_JSON_CONTENT, ResultCodes::INVALID_SYNTAX, ResultCodes::USER_CAN_NOT_BE_UPDATED, ResultCodes::USER_CAN_NOT_BE_DELETED, ResultCodes::USER_CAN_NOT_BE_SAVED => Response::HTTP_BAD_REQUEST,
+            ResultCodes::INVALID_MEDIA_TYPE => Response::HTTP_UNSUPPORTED_MEDIA_TYPE,
+            default => Response::HTTP_INTERNAL_SERVER_ERROR,
+        };
     }
 
     /**
@@ -69,6 +55,7 @@ class BaseResponse
     /**
      * @return array
      */
+    #[ArrayShape(['code' => "int", 'entities' => "array"])]
     public function presentResponse(): array
     {
         return [
